0cd4ac11daeb009eb590d0f3e8e19caa1c61f5cd
Ankit Mishra <ankitsocial1103@gmail.com>
2025-07-07 12:26:58 +0530

    debug deployment

--- app/api/YT-Trailers/route.js
+++ app/api/YT-Trailers/route.js
@@ -1,29 +1,25 @@
 import { NextResponse } from "next/server";
-import { Pool } from "pg";  
+import { Pool } from "pg";
 import "dotenv/config";
 
-const pool=new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-    rejectUnauthorized: false, 
-  },
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 export async function GET() {
-    try {
-        const result = await pool.query(
-            "SELECT * FROM trailers ORDER BY published_at DESC"
-        );
-        return NextResponse.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        return NextResponse.json(
-            { error: "Database fetch failed" },
-            { status: 500 }
-        );
-    }
-    
+  try {
+    const result = await pool.query(
+      "SELECT * FROM trailers ORDER BY published_at DESC"
+    );
+    return NextResponse.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Database fetch failed" },
+      { status: 500 }
+    );
+  }
 }
